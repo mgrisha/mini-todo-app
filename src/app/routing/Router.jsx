48bd9b8dfdcd7cccd3c56f0271a283df72c60a56
Grisha <grisha.endorphone@gmail.com>
2026-03-29 22:19:04 +0300

    fix router

--- src/app/routing/Router.jsx
+++ src/app/routing/Router.jsx
@@ -4,25 +4,25 @@ import { BASE_URL } from "@/shared/constants";
 const getCurrentPath = () => {
 	const pathname = window.location.pathname;
 
-	return pathname.startsWith(BASE_URL) ? pathname.slice(BASE_URL.length - 1) || '/' : pathname;
+	return pathname.startsWith(BASE_URL) ? (pathname.slice(BASE_URL.length - 1) || '/') : pathname;
 }
 
 const matchPath = (path, route) => {
 	const pathParts = path.split("/");   // '/tasks/123' => ['', 'tasks', '123']
-	const routePaths = route.split("/"); // '/tasks/:id' => ['', 'tasks', ':id']
+	const routeParts = route.split("/"); // '/tasks/:id' => ['', 'tasks', ':id']
 
-	if (pathParts.length !== routePaths.length) {
+	if (pathParts.length !== routeParts.length) {
 		return null;
 	}
 
 	const params = {}
 
-	for (let i = 1; i < routePaths.length; i++) {
-		if (routePaths[i].startsWith(':')) {
-			const paramName = routePaths[i].slice(1);
+	for (let i = 1; i < routeParts.length; i++) {
+		if (routeParts[i].startsWith(':')) {
+			const paramName = routeParts[i].slice(1);
 
 			params[paramName] = pathParts[i];
-		} else if (routePaths[i] !== pathParts[i]) {
+		} else if (routeParts[i] !== pathParts[i]) {
 			return null;
 		}
 	}
@@ -31,12 +31,11 @@ const matchPath = (path, route) => {
 }
 
 export const useRoute = () => {
-	const currentPath = getCurrentPath();
-	const [path, setPath] = useState(currentPath);
+	const [path, setPath] = useState(getCurrentPath());
 
 	useEffect(() => {
 		const onLocationChange = () => {
-			setPath(currentPath);
+			setPath(getCurrentPath());
 		}
 
 		window.addEventListener('popstate', onLocationChange);
